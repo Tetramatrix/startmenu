fe7e824e124ae30b6deaff458c3fb88d6569843d
Tetramatrix <chibox@gmail.com>
2024-10-03 10:52:14 +0200

    homepage

--- update.bat
+++ update.bat
@@ -1,4 +1,4 @@
-java -jar "E:\Benutzer\Downloads\compiler-latest\closure-compiler-v20200315.jar" --js "E:\Benutzer\Dokumente\Projects\startmenu\masonry.js" --js_output_file "E:\Benutzer\Dokumente\Projects\startmenu\masonry.min.js"
+java -jar "E:\Benutzer\Downloads\compiler-latest\closure-compiler-v20240317.jar" --js "E:\Benutzer\Dokumente\Projects\startmenu\masonry.js" --js_output_file "E:\Benutzer\Dokumente\Projects\startmenu\masonry.min.js"
 copy "masonry.min.js" "E:\Benutzer\github\startmenu\docs\"
 cd /D "E:\Benutzer\github\startmenu\"
 dir
